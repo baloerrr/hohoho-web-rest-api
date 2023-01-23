564e5c66c89d2fcbc67e79e2d5f97b97740f3bd0
Muhammad Akbar Adityah <99787895+baloerrr@users.noreply.github.com>
2023-01-24 05:32:38 +0700

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,11 +5,7 @@ const toId = mongoose.Types.ObjectId
 
 const createJoke = async(req,res) => {
     const { content } = req.body;
-    // const { id } = req.params
-
-    // const newId = mongoose.Schema.Types.ObjectId.fromString(id)
-
-    // console.log(req.user);
+    
     const newPost = await Post({
          content: content,
          author: req.user
@@ -22,18 +18,6 @@ const createJoke = async(req,res) => {
         console.log(error.message);
     }
 }
-
-// const getJoke = async(req,res) => {
-//     try {
-//         const author = toId(req.params.authorId);
-//         const post = await Post.findById(req.params.postId);
-//         post.author = author;
-//         await post.save();
-//         res.json(post)
-//     } catch (error) {
-//         res.json(error.message);
-//     }
-// }
 
 const getJokes = async(req,res) => {
     try {
